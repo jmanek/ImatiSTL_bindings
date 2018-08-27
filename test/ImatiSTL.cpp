392ae68dfadb08e88e23cce4a39fbeb9188509d0
jmanek <jesse.manek@gmail.com>
2018-08-26 22:26:27 -0700

    build, fails on box

--- test/ImatiSTL.cpp
+++ test/ImatiSTL.cpp
@@ -3,16 +3,18 @@
 #include <igl/readOBJ.h>
 #include <igl/writeOBJ.h>
 #include "exact_outer_hull.h"
-
+#include <iostream>
 int main() { 
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-    igl::readOBJ("diamond_packed.obj", V, F);
+    igl::readOBJ("../test/box.obj", V, F);
     IMATI_STL::TriMesh T;
     Eigen::MatrixXd VV;
     Eigen::MatrixXi FF;
-    exact_outer_hull(V,F,VV,FF);
-    igl::writeOBJ("diamond_packed_out_libigl.obj", VV, FF);
+    std::cout<< V.rows() <<std::endl;
+    std::cout<< F.rows() <<std::endl;
+    exact_outer_hull(V,F,VV,FF, 0.0001);
+    // igl::writeOBJ("../test/box_out.obj", VV, FF);
 
     return 0;
     
