4fe139181a44373ed462c1b5153327f2b86f5862
jmanek <jesse.manek@gmail.com>
2018-08-27 13:09:40 -0700

    Test all ops

--- include/mesh_to_trimesh.h
+++ include/mesh_to_trimesh.h
@@ -13,6 +13,7 @@ namespace imatistl {
         const Eigen::PlainObjectBase<DerivedF> & F,
         IMATI_STL::TriMesh & T
     ) {
+        T = IMATI_STL::TriMesh();
         auto **EV = (IMATI_STL::ExtVertex **)malloc(sizeof(IMATI_STL::ExtVertex *) * V.rows());
         for(int ii = 0; ii < V.rows(); ii++) {
             auto *v = T.newVertex(V(ii, 0), V(ii, 1), V(ii, 2));
--- test/ImatiSTL.cpp
+++ test/ImatiSTL.cpp
@@ -3,27 +3,36 @@
 #include <igl/readOBJ.h>
 #include <igl/writeOBJ.h>
 #include "exact_outer_hull.h"
+#include "cut_intersections.h"
+#include "fill_small_boundaries.h"
+#include "mesh_to_trimesh.h"
+#include "trimesh_to_mesh.h"
 #include <iostream>
 
 int main() { 
-    
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
     igl::readOBJ("../test/spheres.obj", V, F);
-    IMATI_STL::TriMesh T;
-    imatistl::mesh_to_trimesh(V, F, T);
     std::cout << "Vertices: " << V.rows() << std::endl;
     std::cout << "Faces: " << F.rows() << std::endl;
-    T.printReport();
     
+    
+    IMATI_STL::TriMesh T;
+    imatistl::mesh_to_trimesh(V, F, T);
+    T.printReport();
+    imatistl::cut_intersections(T);
+    T.printReport();
+    imatistl::mesh_to_trimesh(V, F, T);
+    imatistl::fill_small_boundaries(T);
+    T.printReport();
+    imatistl::mesh_to_trimesh(V, F, T);
     imatistl::exact_outer_hull(T, 0.01);
+    T.printReport();
+
     Eigen::MatrixXd VV;
     Eigen::MatrixXi FF;
     imatistl::trimesh_to_mesh(T, VV, FF);
     std::cout << "Vertices: " << VV.rows() << std::endl;
     std::cout << "Faces: " << FF.rows() << std::endl;
-    T.printReport();
-    
     igl::writeOBJ("spheres_outer_hull.obj", VV, FF);
-    return 0;
 }
