e5cbdcd1ed7b28ef2b2fefaf5ea4794accc4a719
jmanek <jesse.manek@gmail.com>
2018-08-26 23:08:58 -0700

    box computing

--- test/ImatiSTL.cpp
+++ test/ImatiSTL.cpp
@@ -13,8 +13,8 @@ int main() {
     Eigen::MatrixXi FF;
     std::cout<< V.rows() <<std::endl;
     std::cout<< F.rows() <<std::endl;
-    exact_outer_hull(V,F,VV,FF, 0.0001);
-    // igl::writeOBJ("../test/box_out.obj", VV, FF);
+    exact_outer_hull(V,F,VV,FF, 0.01);
+    igl::writeOBJ("box_outer_hull.obj", VV, FF);
 
     return 0;
     
