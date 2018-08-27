09d50463e03ecc5df8b2d2791135fea5aa0c7661
jmanek <jesse.manek@gmail.com>
2018-08-27 12:32:48 -0700

    test

--- test/ImatiSTL.cpp
+++ test/ImatiSTL.cpp
@@ -6,17 +6,24 @@
 #include <iostream>
 
 int main() { 
+    
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
     igl::readOBJ("../test/spheres.obj", V, F);
     IMATI_STL::TriMesh T;
-    Eigen::MatrixXd VV;
-    Eigen::MatrixXi FF;
+    imatistl::mesh_to_trimesh(V, F, T);
     std::cout << "Vertices: " << V.rows() << std::endl;
     std::cout << "Faces: " << F.rows() << std::endl;
-    imatistl::exact_outer_hull(V,F,VV,FF, 0.01);
+    T.printReport();
+    
+    imatistl::exact_outer_hull(T, 0.01);
+    Eigen::MatrixXd VV;
+    Eigen::MatrixXi FF;
+    imatistl::trimesh_to_mesh(T, VV, FF);
     std::cout << "Vertices: " << VV.rows() << std::endl;
     std::cout << "Faces: " << FF.rows() << std::endl;
+    T.printReport();
+    
     igl::writeOBJ("spheres_outer_hull.obj", VV, FF);
     return 0;
 }
